f435ca153eeb25c862439f7a6cc14cb0fd394e65
Mulyono Putra <mulyonoputra27@outlook.com>
2022-06-03 15:35:10 +0700

    Init Project with Tailwind, Atomic Design etc.

---
+++ src/pages/main/index.js
@@ -0,0 +1,13 @@
+
+import ProductList from 'pages/product-list';
+import { Header } from '../../components/molecules/index';
+
+const Main = () => {
+  return (
+    <>
+      <Header />
+      <ProductList/>
+    </>
+  );
+};
+export default Main;
