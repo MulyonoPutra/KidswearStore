bd6ac0796de7fa5722798675bfbfc14485c84b9c
Mulyono Putra <mulyonoputra27@outlook.com>
2022-06-03 19:36:09 +0700

    Refactor into CSS

--- src/components/molecules/header/index.js
+++ src/components/molecules/header/index.js
@@ -34,34 +34,14 @@ const Header = () => {
           <Popover.Group as='nav' className='hidden md:flex space-x-10'>
             {/*   <DropdownMenu/> */}
 
-            <a
-              href='!#'
-              className='text-base font-medium text-gray-500 hover:text-gray-900'
-            >
-              Pricing
-            </a>
-            <Link
-              to='/shipping'
-              className='text-base font-medium text-gray-500 hover:text-gray-900'
-            >
-              Docs
-            </Link>
+            <Link to='/shipping' className='link'> Pricing </Link>
+            <Link to='/shipping' className='link'> Docs </Link>
 
             {/* <MoreMenu/> */}
           </Popover.Group>
-          <div className='hidden md:flex items-center justify-end md:flex-1 lg:w-0'>
-            <a
-              href='!#'
-              className='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900'
-            >
-              Sign in
-            </a>
-            <a
-              href='!#'
-              className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'
-            >
-              Sign up
-            </a>
+          <div className='wrapper-btn'>
+            <Link to='/shipping' className='signin'> Sign in </Link>
+            <Link to='/shipping' className='signup'> Sign up </Link>
           </div>
         </div>
       </div>
