56b0b54ebbe756d0e60e56c0d17c28a4171900bb
Mulyono Putra <mulyonoputra27@outlook.com>
2022-06-19 00:01:13 +0700

    Update user.action.js

--- src/config/redux/action/user.action.js
+++ src/config/redux/action/user.action.js
@@ -7,7 +7,7 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAILURE,
-} from './../../constants/user.constant';
+} from '../../constants/user.constant';
 
 export const signin = (email, password) => async (dispatch) => {
   dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
@@ -26,34 +26,32 @@ export const signin = (email, password) => async (dispatch) => {
   }
 };
 
-export const register = (name, email, password) => {
-  return async (dispatch) => {
-    dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
-    try {
-      const { data } = await axios.post('/v1/user/register', {
-        name,
-        email,
-        password,
-      });
-      dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-      dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
-      localStorage.setItem('userInfo', JSON.stringify(data));
-    } catch (error) {
-      dispatch({
-        type: USER_REGISTER_FAILURE,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      });
-    }
-  };
+export const register = (name, email, password) => async (dispatch) => {
+  dispatch({ type: USER_REGISTER_REQUEST, payload: { email, password } });
+  try {
+    const { data } = await axios.post('/v1/user/register', {
+      name,
+      email,
+      password,
+    });
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
 
-export const signout = () => {
-  return (dispatch) => {
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('cartItems');
-    dispatch({ type: USER_SIGNOUT_REQUEST });
-  };
+export const signout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  localStorage.removeItem('cartItems');
+  localStorage.removeItem('shippingAddress');
+  dispatch({ type: USER_SIGNOUT_REQUEST });
+  document.location.href = '/login';
 };
